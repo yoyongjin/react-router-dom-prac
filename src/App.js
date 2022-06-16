b0cbfda2be3c9ccd3a3ef62790ac320623b49f18
yoyongjin <yongjinalize@gmail.com>
2022-06-16 18:14:44 +0900

    라우팅 순서, exact prop을 통한 라우팅 관리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { Switch } from "react-router-dom";
 import { Route } from "react-router-dom";
 import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
 import Products from "./pages/Products";
 import Welcome from "./pages/Welcome";
 
@@ -8,14 +10,20 @@ function App() {
     <div>
       <MainHeader />
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        {/* domain.com/welcome => Welcome Component */}
-        <Route path="/products">
-          <Products />
-        </Route>
-        {/* domain.com/products => Products Component */}
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          {/* domain.com/welcome => Welcome Component */}
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          {/* domain.com/products => Products Component */}
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
+        {/* domain.com/product-detail/~~~  */}
       </main>
     </div>
   );
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Products = () => {
-  return <h1>Products Page</h1>;
+  return (
+    <section>
+      <h1>Products Page</h1>
+      <ul>
+        <li>
+          <Link to="/products/p1">Product 1</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">Product 2</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">Product 3</Link>
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
