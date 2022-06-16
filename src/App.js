5a4257a9f988bd9affe690beacaec28f1b33a521
yoyongjin <yongjinalize@gmail.com>
2022-06-16 17:09:09 +0900

    BrowserRouter, Route, Link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import { Route } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        {/* domain.com/welcome => Welcome Component */}
+        <Route path="/products">
+          <Products />
+        </Route>
+        {/* domain.com/products => Products Component */}
+      </main>
     </div>
   );
 }
---
+++ src/pages/Products.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Products = () => {
+  return <h1>Products Page</h1>;
+};
+
+export default Products;
